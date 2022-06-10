f10e1a34860ca34b44c6965d9e00412cb7fe8153
Clyde1063 <clyde.apples@gmail.com>
2022-06-10 18:01:04 +0200

    made the hamburger a button for desktop

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -19,10 +19,12 @@ const Sidebar = () => {
       <nav class="navbar navbar-expand-lg bg-light">
          <div class="container-fluid">
             <a class="navbarMain navbar-brand" href="/">Navbar</a>
-            <GiHamburgerMenu 
-               className="hamburger-menu-desktop"
-               type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
-            />
+            <button style={{backgroundColor: 'black'}}>
+               <GiHamburgerMenu
+                  className="hamburger-menu-desktop"
+                  type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
+               />
+            </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                   <li class="nav-item">
